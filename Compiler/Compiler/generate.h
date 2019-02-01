f847c9095037c17980fc492d36339f3d27251e67
Elijah Johnson <elijahjohnson987@gmail.com>
2019-01-31 20:47:44 -0700

    Added unary operator support

--- Compiler/Compiler/generate.h
+++
@@ -1,8 +0,0 @@
-#ifndef GENERATE_H
-#define GENERATE_H
-
-char *generateReturnExpr(struct ASTNode *statement);
-char *generateFunction(struct ASTNode *function);
-char *generateAssembly(struct ASTNode *root);
-
-#endif // !GENERATE_H
--- Compiler/Compiler/lexer.h
+++
@@ -1,8 +0,0 @@
-#ifndef LEXER_H
-#define LEXER_H
-
-char *readFileToString(FILE *f);
-struct TokenList* tokenizeFile(char *fileString, int *tokenCount);
-void removeSpaces(char *source);
-
-#endif // !LEXER_H
